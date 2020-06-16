8e96ca5129ba64580e7a286314d64b4555a8cedc
Anderson <upmalagutti@gmail.com>
2020-06-16 11:38:12 +0200

    added file with arrays, dictionaries and tuples

---
+++ arrayDictionaryTuple.swift
@@ -0,0 +1,51 @@
+/**
+* 1. Create an array called shopItemsArray and fill it with a few strings, then create a dictionary called shopItemsDictionary with keys that match your array items, with prices for their values
+* 2. Use the contains method to check if shopItemsArray has the item you want (make it an item that is not in the array)
+* 3. Use the insert method to add the missing item to shopItemsArray at an index you choose
+* 4. Create a new variable called selectedItem and assign it an item from shopItemsArray by using subscript syntax.
+* 5. Create another variable called selectedItemPrice and use selectedItem and subscript syntax to access it's value from shopItemsDictionary
+* 6. Create a set called fullArmorSet and assign it a few string values, then create a second set called currentArmorSet and assign it some, but not all, of the same values as fullArmorSet
+* 7. Use the subtract method to find out which items currentArmorSet is missing for a complete set
+* 8. Create a tuple called armorPieceTuple and assign it named variables for name, cost, and canEquip, all with initial values. Then access the canEquip value with dot notation.
+*/
+
+// 1
+var shopItemsArray : [String] = ["Shield", "Sword", "Potion"];
+print(shopItemsArray);
+var shopItemsDictionary : [String : Int] = [
+    shopItemsArray[0] : 100,
+    shopItemsArray[1]: 200,
+    shopItemsArray[2]: 50,
+    "Bag": 80
+];
+print(shopItemsDictionary);
+
+// 2
+shopItemsArray.contains("Shield");
+shopItemsArray.contains("Bag");
+
+// 3
+//shopItemsArray.append("Bag");
+shopItemsArray.insert("Bag", at: 1);
+print(shopItemsArray);
+shopItemsArray.contains("Bag");
+
+// 4
+var selectedItem : String = shopItemsArray[2];
+print(selectedItem);
+
+// 5
+var selectedItemPrice = shopItemsDictionary[selectedItem];
+//print(selectedItemPrice!);
+
+// 6
+var fullArmorSet : Set<String> = ["Helmet", "Shoulder Protection", "Pants", "Boots"];
+var currentArmorSet : Set = ["Shoulder Protection", "Pants"];
+
+// 7
+var missingArmorSet : Set = fullArmorSet.subtracting(currentArmorSet);
+print(missingArmorSet);
+
+// 8
+var armorPieceTuple = (name: "Golden Helmet", cost: 800, canEquip: true);
+print(armorPieceTuple.canEquip);
